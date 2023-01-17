2cc4888f8fbb224196dacf3da64b2a8ed04c75fc
Per Jansson <per.jansson@reaktor.com>
2023-01-17 11:26:14 +0200

    Add joke restaurant by Sauli until end of January

--- app/routes/slack/day.ts
+++ app/routes/slack/day.ts
@@ -1,5 +1,5 @@
 import type { ActionFunction } from "@remix-run/node"
-import { Recommendation, Restaurant } from "~/types"
+import type { Recommendation, Restaurant } from "~/types"
 import { getShortestDirectionsInTime } from "~/utils/google"
 import { loader as indexLoader } from "../index"
 
@@ -7,6 +7,8 @@ interface LoaderData {
   response_type?: "in_channel"
   text: string
 }
+
+let jokeCounter = 0
 
 export const action: ActionFunction = async (
   dataFunctionArgs
@@ -27,13 +29,42 @@ export const action: ActionFunction = async (
       }
     }
 
-    const restaurant =
+    let restaurant =
       existingRecommendationForToday?.Restaurant ||
       randomRestaurantToRecommendForToday
 
     if (!restaurant) {
       return {
         text: "Oh no, there was no restaurant randomized. Perhaps you can go to https://lunch-web.fly.dev and randomize one?",
+      }
+    }
+
+    // TEMPORARY BLANKO & KELLARI JOKE BY SAULI AND PER
+    const currentDate = new Date()
+    const endJokeDate = new Date("2023-02-01")
+
+    if (currentDate < endJokeDate) {
+      const jokeRestaurant =
+        jokeCounter % 2 === 1
+          ? {
+              name: "Kellari",
+              address: "Linnankatu 16, 20100 Turku",
+              time: "1 min",
+              distance: "84m",
+            }
+          : {
+              name: "Blanko",
+              address: "Aurakatu 1, 20100 Turku",
+              time: "2 min",
+              distance: "200m",
+            }
+
+      jokeCounter++
+
+      return {
+        response_type: "in_channel",
+        text: `Today's lunch recommendation is *${jokeRestaurant.name}* on ${jokeRestaurant.address}.\n
+It takes ${jokeRestaurant.time} to walk the ${jokeRestaurant.distance} to the restaurant. Check out map directions at https://lunch-web.fly.dev. Bon appetit! :chefs-kiss:`,
       }
     }
 
